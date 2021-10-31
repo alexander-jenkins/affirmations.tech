6925fe1c7eaa302a4ed0f81858a55536aca89cfc
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2021-10-31 05:10:01 -0400

    added 20 messages to testing array

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import 'react-phone-number-input/style.css';
 import PhoneInputField from './PhoneInput';
 
 // testing array of messages
-const messages = ['You are amazing!', "Don't give up!"];
+const messages = [
+  'You are worth it!',
+  'Take some time to reflect on all that you have accomplished today.',
+  'Taking a day to relax and recharge IS productive.',
+  'When life gives you lemons, make the best lemonade that the world has ever tasted.',
+  'You are a gift to those around you.',
+  'Everything will fall into place, sometimes it just takes time.',
+  'Give yourself space to grow and learn.',
+  'The world would not be the same without you.',
+  'Just be yourself.',
+  'Pick yourself back up and turn it around.',
+  'Can I offer you a nice egg in this frying time?',
+  'You are strong.',
+  'You will do great things.',
+  'Today is a great day!',
+  'Everything will all work out.',
+  'You are a blessing to those around you.',
+  'Take pride in everything that you have accomplished.',
+  'You ARE good enough!',
+  "You've got it in you.",
+  'Every little thing is gonna be alright.',
+];
 
 function App() {
   // declare state vars
