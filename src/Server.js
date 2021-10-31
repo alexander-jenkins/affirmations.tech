781fc37fb9a515d5e2efa753406ce7a627c943ea
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2021-10-31 12:13:58 -0400

    adjust api

--- src/Server.js
+++ src/Server.js
@@ -13,7 +13,14 @@ const client = require('twilio')(
 
 // set up the express app
 const app = express();
-app.use(cors());
+app.use(cors(), function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://api.affirmations.tech:3001'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 const port = process.env.REACT_APP_EXPRESS_PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
