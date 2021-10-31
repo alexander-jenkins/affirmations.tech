2d1194e0f37adb2f7b98da732823f7a45978512c
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2021-10-30 23:40:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 function App() {
+  // declare state vars
+  const [number, setNumber] = useState('');
+  const [body, setBody] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <h2>Send an Affirmation</h2>
+        <Form>
+          <Form.Group>
+            <Form.Label htmlFor='to'>To</Form.Label>
+            <Form.Control
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label htmlFor='message'>Body</Form.Label>
+            <Form.Control
+              as='textarea'
+              rows='3'
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+        <Button variant='primary' type='submit'>
+          Send
+        </Button>
+      </Container>
+      {console.log(`Number is ${number} and the Message is ${body}`)}
     </div>
   );
 }
