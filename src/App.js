d869e3afe02cc9f5eefba2dd0af76b7a280070a5
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2021-10-31 08:41:14 -0400

    fix server url

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function MessageForm({ messages, phoneNumber, setPhoneNumber }) {
 
     // send message
     console.log(`to: ${phoneNumber}, body: ${message}`);
-    fetch('/api/send-affirmation', {
+    fetch('http://161.35.123.179:5000/api/send-affirmation', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
