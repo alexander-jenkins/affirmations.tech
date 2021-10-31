b53d88a41921d7136f0b1e55be2cd96bc096f822
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2021-10-31 01:07:28 -0400

    added skeleton of the message sending form

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React, { useState } from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import 'react-phone-number-input/style.css';
+import PhoneInputField from './PhoneInput';
 
 function App() {
   // declare state vars
-  const [number, setNumber] = useState('');
-  const [body, setBody] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState();
+  const sendMessage = () => {
+    console.log(`Sending a message to ${phoneNumber}`);
+  };
 
   return (
     <div className='App'>
       <Container>
         <h2>Send an Affirmation</h2>
-        <Form>
-          <Form.Group>
-            <Form.Label htmlFor='to'>To</Form.Label>
-            <Form.Control
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group>
-            <Form.Label htmlFor='message'>Body</Form.Label>
-            <Form.Control
-              as='textarea'
-              rows='3'
-              value={body}
-              onChange={(e) => setBody(e.target.value)}
-            />
-          </Form.Group>
-        </Form>
-        <Button variant='primary' type='submit'>
-          Send
-        </Button>
+        <PhoneInputField
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+        ></PhoneInputField>
+        <button onClick={sendMessage}>Send Affirmation</button>
       </Container>
-      {console.log(`Number is ${number} and the Message is ${body}`)}
+
+      {console.log(`Phone number is: ${phoneNumber}`)}
     </div>
   );
 }
